d6c4fd26f61edf5d42da5024e79345d7c9fea7ab
quadrimuzammil <97599672+quadrimuzammil@users.noreply.github.com>
2023-01-17 12:52:09 +0530

    Update service-account.tf

--- service-account.tf
+++ service-account.tf
@@ -14,5 +14,5 @@ resource "google_project_iam_member" "service-a" {
 resource "google_service_account_iam_member" "service-a" {
   service_account_id = google_service_account.service-a.id
   role               = "roles/iam.workloadIdentityUser"
-  member             = "serviceAccount:canvas-rampart-364906.svc.id.goog[staging/service-a]"
+  member             = "serviceAccount:devops-374009.svc.id.goog[staging/service-a]"
 }
